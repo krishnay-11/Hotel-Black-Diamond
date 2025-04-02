5f9e3cf00fd38d7a31219ae5c5bc6d26813e535e
krishnay-11 <147747968+krishnay-11@users.noreply.github.com>
2025-04-02 18:54:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/send-email', async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running at PORT:${PORT}`);
 });
